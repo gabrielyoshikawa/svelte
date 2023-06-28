a12481dbc27e4f976ad29781fb1cea2023bd8811
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2023-06-28 20:39:53 -0300

    Feature - new page

---
+++ ui/src/lib/Loops.svelte
@@ -0,0 +1,50 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+
+    let fruits = [
+        { id: 1, name: "Banana", color: "yellow", quantity: 24 },
+        { id: 2, name: "Avocado", color: "green", quantity: 15 },
+        { id: 3, name: "Tangerine", color: "orange", quantity: 56 }
+    ];
+</script>
+
+<div class="content">
+    <Navbar />
+
+    <div class="box">
+        <!-- Good practice - pass a unique value as parameter -->
+        {#each fruits as fruit (fruit.id)}
+            <p>{fruit.name} - {fruit.quantity} units in stock.</p>
+        {:else}
+            <p>We sell it all! No fruit available in stock.</p>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .content {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .box {
+        width: 35%;
+        height: 85%;
+        background-color: rgb(204, 55, 55);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin-top: 1.1rem;
+    }
+
+    p {
+        font-size: 1.5em;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+            sans-serif;
+    }
+</style>
