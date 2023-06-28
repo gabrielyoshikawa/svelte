9f36d514fcabe297d714c4450be0d5cfc1233e0c
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2023-06-28 20:39:37 -0300

    Feature - Homepage

---
+++ ui/src/lib/HomePage.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<div class="content">
+    <Navbar />
+
+</div>
+
+<style>
+    .content {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
