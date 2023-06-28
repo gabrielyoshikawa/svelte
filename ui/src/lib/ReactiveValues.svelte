12b6c2524e8b2f61ee6d6d37a1591e2762426495
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2023-06-28 20:39:57 -0300

    Feature - new page

--- ui/src/lib/ReactiveValues.svelte
+++ ui/src/lib/ReactiveValues.svelte
@@ -1,14 +1,68 @@
 <script>
     import Navbar from "../components/Navbar.svelte";
+
+    let firstName = "John";
+    let lastName = "Doe";
+    let beltColor = "Black";
+
+    // Reactive value
+    $: fullName = `${firstName} ${lastName}`;
+
+    // Reactive statement
+    $: {
+        console.log(fullName)
+    }
+
 </script>
 
 <div class="content">
     <Navbar />
+
+    <div class="box">
+        <!-- <p>{firstName} {lastName} - your belt color is: {beltColor}</p> -->
+        <!-- Work the same way as above, but using reactive value :D -->
+        <p>{fullName} - your belt color is: {beltColor}</p>
+        <input type="text" bind:value={firstName} />
+        <input type="text" bind:value={lastName} />
+        <input type="text" bind:value={beltColor} />
+    </div>
 </div>
 
 <style>
     .content {
         width: 100vw;
         height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .box {
+        width: 35%;
+        height: 85%;
+        background-color: rgb(130, 97, 163);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin-top: 1.1rem;
+    }
+
+    p {
+        font-size: 2em;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+            sans-serif;
+    }
+
+    input {
+        margin-top: 1rem;
+        height: 5%;
+        width: 40%;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+            sans-serif;
+        border: none;
+        background-color: rgb(223, 149, 210);
     }
 </style>
