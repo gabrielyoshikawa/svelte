7464faa1528fa954eb8b1aab63de59875110168c
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2023-07-01 20:42:12 -0300

    Feature - Conditional style

--- ui/src/lib/Conditionals.svelte
+++ ui/src/lib/Conditionals.svelte
@@ -1,21 +1,39 @@
 <script>
     import Navbar from "../components/Navbar.svelte";
 
+    // Conditional html
     let num = 5;
+    let showModal = true;
+
+    // Conditional style
+    let advertisement = true;
 </script>
 
 <div class="content">
     <Navbar />
 
-    <div class="box">
-        {#if num > 20}
-            <p>Greater than 20</p>
-        {:else if num > 5}
-            <p>Greater than 5</p>
-        {:else}
-            <p>Not greater than 5</p>
-        {/if}
-        
+    <div class="contentAlignment">
+
+        <div class="box">
+            {#if num > 20}
+                <p>Greater than 20</p>
+            {:else if num > 5}
+                <p>Greater than 5</p>
+            {:else}
+                <p>Not greater than 5</p>
+            {/if}
+        </div>
+
+        <div class="box">
+            {#if showModal}
+                <div class="backdrop" class:advertisement={advertisement}>
+                    <div class="modal">
+                        <p>Sign in for offers!</p>
+                    </div>
+                </div>
+            {/if}
+        </div>
+    
     </div>
 </div>
 
@@ -24,13 +42,19 @@
         width: 100vw;
         height: 100vh;
         display: flex;
-        align-items: center;
         flex-direction: column;
+    }
+
+    .contentAlignment {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-evenly;
     }
 
     .box {
         width: 35%;
-        height: 85%;
+        height: 95%;
         background-color: rgb(102, 180, 92);
         display: flex;
         justify-content: center;
@@ -41,8 +65,27 @@
 
     p {
         font-size: 1.5em;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
-            Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-            sans-serif;
+    }
+
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal {
+        width: 35%;
+        height: 12%;
+        background-color: white;
+        display: flex;
+        border-radius: 5%;
+        text-align: center;
+    }
+
+    .advertisement .modal {
+        background-color: crimson;
     }
 </style>
